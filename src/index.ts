ac6ea70121586a8bc340dd1dbac23669f7bc226e
Jesus Sugar <jesussugar27@gmail.com>
2026-06-21 21:54:53 -0500

    connecting the backend with the data base

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
+import pool from './db'
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
 
 const app = express()
 app.use(cors())
@@ -9,6 +12,12 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000
 
+pool.query('SELECT NOW()')
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.error('DB connection error:', err))
+
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
 })
+
+
